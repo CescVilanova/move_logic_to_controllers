34d6db165703d5b46d312250e9e4cdd169ce3833
CescVilanova <vfrancesc@gmail.com>
2014-02-20 15:18:02 +0000

    Last exercise almost finished (destroy not working)

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -27,5 +27,58 @@ class ProductsController < ApplicationController
     @product = Product.new
   end
 
+  def show
+    @product = Product.where(:id => params[:id]).first
+  end
+
+  def edit
+    @product = Product.where(:id => params[:id]).first
+  end
+
+  def update
+    @product = Product.where(:id => params[:id]).first
+
+    respond_to do |format|
+      if @product.update_attributes(params[:product])
+        format.html { redirect_to @product, :notice => 'Product was successfully updated.' }
+        format.json { head :no_content }
+      else 
+        format.html { render action: "edit" }
+        format.json { render json: @product.errors, :status => :unprocessable_entity }
+      end
+    end
+  end
+
+  def destroy
+    @product = Product.where(:id => params[:id]).first
+    @product.destroy!
+
+    respond_to do |format| 
+      format.html { redirect_to products_url }
+      format.json { head :no_content}
+    end
+  end
+
+
+# Alernative method to discover what's going wrong. 
+
+#  def destroy
+#   @product = Product.find(params[:id])
+#     Rails.logger.info ">>>> Product: #{@product.inspect}"
+#     Rails.logger.info ">>>> You have #{Product.count} products before destroy."
+#       if @product.destroy
+#         Rails.logger.info ">>>> You now have #{Product.count} products. Destroying success? #{success}."
+#         flash[:notice] = "The product was destroyed."
+#       else
+#         Rails.logger.info ">>>> Whoops! Product not destroyed!"
+#         flash[:error] = "Product couldn't be destroyed!"
+#       end
+
+#       respond_to do |format| 
+#         format.html { redirect_to products_url }
+#         format.json { head :no_content}
+#     end
+#   end
+
 
 end
